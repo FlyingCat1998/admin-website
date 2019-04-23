5ac68d924ce479e97606c8d18e068ecd512b2874
Flyingcat1998 <Flyingcat1998@gmail.com>
2019-04-23 20:42:51 +0700

    Thêm phần quản lý các tài khoản người dùng và chỉnh lại file layout.hbs

--- routes/index.js
+++ routes/index.js
@@ -10,5 +10,8 @@ router.get('/trang-chu.hbs', function(req, res, next) {
   res.render('trang-chu', { title: 'DoubleT\'s Admin website' });
 });
 
+router.get('/danh-sach-nguoi-dung.hbs', function(req, res, next) {
+  res.render('danh-sach-nguoi-dung', { title: 'DoubleT | Danh sách người dùng' });
+});
 
 module.exports = router;
